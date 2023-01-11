6f23492e1dc37aed71b7ff07ec689f2b05e87d88
Adrian Antanon <adrianantanon@gmail.com>
2023-01-11 18:05:47 +0100

    STYLES: add dark & light theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,23 @@
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 import '../styles/globals.css';
+import { lightTheme, darkTheme } from '../themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    // <NextThemesProvider
+    //   defaultTheme="system"
+    //   attribute="class"
+    //   value={{
+    //     light: lightTheme.className,
+    //     dark: darkTheme.className,
+    //   }}
+    // >
+    <NextUIProvider theme={darkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
+    // </NextThemesProvider>
   );
 }
