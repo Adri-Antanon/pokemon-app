ea0e61ba9ffeed51bbb7bf41acd893086620f7af
Adrian Antanon <adrianantanon@gmail.com>
2023-01-11 17:53:45 +0100

    FEAT: install nextui & add _document

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@nextui-org/react';
 import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-  return <h1>pokeApp</h1>;
+  return (
+    <>
+      <h1>pokeApp</h1>
+      <Button color={'gradient'}> Pika pika </Button>
+    </>
+  );
 };
 
 export default HomePage;
