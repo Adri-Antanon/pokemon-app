348525916a1ddfd103fd4595a63d89fe6d00930d
Adrian Antanon <adrianantanon@gmail.com>
2023-01-21 17:26:25 +0100

    REFACTOR: modularize favorite components

--- components/pokemon/index.ts
+++ components/pokemon/index.ts
@@ -1,1 +1,2 @@
 export { default as PokemonCard } from './PokemonCard';
+export { PokemonFavoriteCard } from './PokemonFavoriteCard';
--- components/ui/Favorites.tsx
+++ components/ui/Favorites.tsx
@@ -1,5 +1,6 @@
 import { Card, Grid } from '@nextui-org/react';
 import { FC } from 'react';
+import { PokemonFavoriteCard } from '../pokemon/PokemonFavoriteCard';
 
 interface Props {
   favoritePokemons: number[];
@@ -9,21 +10,7 @@ export const Favorites: FC<Props> = ({ favoritePokemons }) => {
   return (
     <Grid.Container gap={2} direction="row" justify="flex-start">
       {favoritePokemons.map((id) => (
-        <Grid xs={6} sm={3} md={2} key={id}>
-          <Card
-            hoverable
-            clickable
-            css={{
-              padding: 10,
-            }}
-          >
-            <Card.Image
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
-              width={'100%'}
-              height={'140px'}
-            />
-          </Card>
-        </Grid>
+        <PokemonFavoriteCard pokemonId={id} key={id} />
       ))}
     </Grid.Container>
   );
